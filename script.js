b375454980d1e9504a633b6480d6ac7aa73d4eb4
Jane Lutak <foux02911@gmail.com>
2023-11-28 00:47:57 +0300

    Задание полностью выполнено

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+'use strict';
 
+const square = document.getElementById('square');
+const eBtn = document.getElementById('e_btn');
+const circle = document.getElementById('circle');
+const text = document.getElementById('text');
+const range = document.getElementById('range');
+const rangeSpan = document.getElementById('range-span');
 
+const colorText = window.getComputedStyle(text).color;
 
+const changeColor = function () {
+  square.style.backgroundColor = colorText;
+};
 
+const changeHeightWidth = function () {
+  rangeSpan.textContent = event.target.value;
+  circle.style.cssText = `height: ${rangeSpan.textContent}%; width:${rangeSpan.textContent}%`;
+};
 
+square.addEventListener('click', changeColor);
+range.addEventListener('input', changeHeightWidth);
+eBtn.style.cssText = 'display: none';
